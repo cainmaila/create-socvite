0c536f2f0fbe3109d1719681c6eb824bf805826b
cain.chu <cainmaila>
2023-11-23 19:08:10 +0800

    15

---
+++ soc-svelte/src/lib/Router.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-routing";
+  import App from "$lib/view/App.svelte";
+  let url = "";
+</script>
+
+<Router {url}>
+  <Route path="/" component={App} />
+</Router>
