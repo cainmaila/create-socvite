9ba88d9abb970122e8177f169ec16c8fd269f666
cain.chu <cainmaila>
2023-11-23 19:39:03 +0800

    19

--- soc-svelte/vite.config.ts
+++ soc-svelte/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import TurboConsole from 'vite-plugin-turbo-console'
 import { VitePWA } from 'vite-plugin-pwa'
-import { webUpdateNotice } from '@plugin-web-update-notification/vite'
 import path from 'path'
 import config from './package.json'
 
@@ -15,14 +14,6 @@ export default defineConfig({
       disableLaunchEditor: true,
     }),
     VitePWA({ registerType: 'autoUpdate' }),
-    webUpdateNotice({
-      notificationProps: {
-        title: `新版本升級 ${config.version}`,
-        description: '系統更新，請重新整理頁面',
-        buttonText: '重新整理並升級',
-        dismissButtonText: '忽略',
-      },
-    }),
   ],
   resolve: {
     alias: {
